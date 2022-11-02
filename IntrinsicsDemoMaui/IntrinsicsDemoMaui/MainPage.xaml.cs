5be4827fe96d125fe9309a5aa457a6d80b440c29
zhouyuelin <zyl910@sina.com>
2022-11-02 23:08:53 +0800

    IntrinsicsDemoMaui: 调整界面, 放好 Editor

--- IntrinsicsDemoMaui/IntrinsicsDemoMaui/MainPage.xaml.cs
+++ IntrinsicsDemoMaui/IntrinsicsDemoMaui/MainPage.xaml.cs
@@ -13,12 +13,13 @@ public partial class MainPage : ContentPage {
     private void OnCounterClicked(object sender, EventArgs e) {
         count++;
         string msg = string.Format("Vector<Byte>.Count={0}, IsHardwareAccelerated={1}. ", Vector<Byte>.Count, Vector.IsHardwareAccelerated);
-        msg += string.Format("Vector256<Byte>.Count={0}, IsHardwareAccelerated={1}. ", Vector256<Byte>.Count, Vector.IsHardwareAccelerated);
+        msg += string.Format("\nVector256<Byte>.Count={0}, IsHardwareAccelerated={1}. ", Vector256<Byte>.Count, Vector.IsHardwareAccelerated);
+        InfoEditor.Text = msg;
 
         if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time. " + msg;
+            CounterBtn.Text = $"Clicked {count} time. ";
         else
-            CounterBtn.Text = $"Clicked {count} times. " + msg;
+            CounterBtn.Text = $"Clicked {count} times. ";
 
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
