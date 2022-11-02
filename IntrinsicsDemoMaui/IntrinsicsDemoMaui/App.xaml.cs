b41edbeed9d48f08b0b2c78ec7cf6c0153426933
zhouyuelin <zyl910@sina.com>
2022-11-02 22:16:22 +0800

    Add IntrinsicsDemoMaui.

---
+++ IntrinsicsDemoMaui/IntrinsicsDemoMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IntrinsicsDemoMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ IntrinsicsDemoMaui/IntrinsicsDemoMaui/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics;
+
+namespace IntrinsicsDemoMaui;
+
+public partial class MainPage : ContentPage {
+    int count = 0;
+
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e) {
+        count++;
+        string msg = string.Format("Vector<Byte>.Count={0}, IsHardwareAccelerated={1}. ", Vector<Byte>.Count, Vector.IsHardwareAccelerated);
+        msg += string.Format("Vector256<Byte>.Count={0}, IsHardwareAccelerated={1}. ", Vector256<Byte>.Count, Vector.IsHardwareAccelerated);
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time. " + msg;
+        else
+            CounterBtn.Text = $"Clicked {count} times. " + msg;
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
