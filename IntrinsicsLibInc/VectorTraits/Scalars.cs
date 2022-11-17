ee450107d4cf7814268af6bdd24ea4c81d56a313
zhouyuelin <zyl910@sina.com>
2022-11-17 21:55:09 +0800

    Add BitUtil .

--- IntrinsicsLibInc/VectorTraits/Scalars.cs
+++ IntrinsicsLibInc/VectorTraits/Scalars.cs
@@ -10,42 +10,6 @@ namespace Zyl.VectorTraits {
     /// </summary>
     public static class Scalars {
 
-        // == MemoryMarshal ==
-
-        /// <summary>
-        /// Returns a reference to the element of the read-only span at index 0 (返回只读跨度在索引0处的引用).
-        /// </summary>
-        /// <typeparam name="T">The type of items in the span (跨度中的元素的类型).</typeparam>
-        /// <param name="span">The read-only span from which the reference is retrieved (用于检索引用的只读跨度).</param>
-        /// <returns>A reference to the element at index 0 (对索引为 0 处元素的引用).</returns>
-        /// <seealso cref="MemoryMarshal.GetReference{T}(ReadOnlySpan{T})"/>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ref T GetReference<T>(ReadOnlySpan<T> span) {
-#if (NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
-            return ref MemoryMarshal.GetReference<T>(span);
-#else
-            return ref Unsafe.AsRef(span.GetPinnableReference());
-#endif // NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-        }
-
-        /// <summary>
-        /// Returns a reference to the element of the span at index 0 (返回跨度在索引0处的引用).
-        /// </summary>
-        /// <typeparam name="T">The type of items in the span (跨度中的元素的类型).</typeparam>
-        /// <param name="span">The span from which the reference is retrieved(用于检索引用的跨度).</param>
-        /// <returns>A reference to the element at index 0 (对索引为 0 处元素的引用).</returns>
-        /// <seealso cref="MemoryMarshal.GetReference{T}(Span{T})"/>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ref T GetReference<T>(Span<T> span) {
-#if (NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
-            return ref MemoryMarshal.GetReference<T>(span);
-#else
-            return ref span.GetPinnableReference();
-#endif // NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-        }
-
-        // == Scalars ==
-
         /// <summary>
         /// Converts a <see cref="double"/> to the target type value (将 <see cref="double"/> 转换为目标类型值).
         /// </summary>
@@ -117,7 +81,7 @@ namespace Zyl.VectorTraits {
         /// <returns>Returns target type value (返回目标类型的值).</returns>
         public static T GetByBits<T>(Int64 src) {
             if (typeof(T) == typeof(Single)) {
-                return (T)(object)Scalars.Int32BitsToSingle((Int32)src);
+                return (T)(object)BitUtil.Int32BitsToSingle((Int32)src);
             } else if (typeof(T) == typeof(Double)) {
                 return (T)(object)BitConverter.Int64BitsToDouble(src);
             } else if (typeof(T) == typeof(SByte)) {
@@ -142,7 +106,7 @@ namespace Zyl.VectorTraits {
                 return (T)(object)(UIntPtr)(ulong)src;
 #if NET5_0_OR_GREATER
             } else if (typeof(T) == typeof(Half)) {
-                return (T)(object)Int16BitsToHalf((Int16)src);
+                return (T)(object)BitUtil.Int16BitsToHalf((Int16)src);
 #endif // NET5_0_OR_GREATER
             } else {
                 return (T)Convert.ChangeType(src, typeof(T));
@@ -157,7 +121,7 @@ namespace Zyl.VectorTraits {
         /// <returns>Returns a <see cref="Int64"/> bits (返回 <see cref="Int64"/> 类型的64位值).</returns>
         public static Int64 GetInt64BitsFrom<T>(T src) where T:struct {
             if (typeof(T) == typeof(Single)) {
-                return (Int64)Scalars.SingleToInt32Bits((Single)(object)src);
+                return (Int64)BitUtil.SingleToInt32Bits((Single)(object)src);
             } else if (typeof(T) == typeof(Double)) {
                 return (Int64)BitConverter.DoubleToInt64Bits((Double)(object)src);
             } else if (typeof(T) == typeof(SByte)) {
@@ -182,130 +146,12 @@ namespace Zyl.VectorTraits {
                 return (Int64)(UIntPtr)(object)src;
 #if NET5_0_OR_GREATER
             } else if (typeof(T) == typeof(Half)) {
-                return (Int64)HalfToInt16Bits((Half)(object)src);
+                return (Int64)BitUtil.HalfToInt16Bits((Half)(object)src);
 #endif // NET5_0_OR_GREATER
             } else {
                 return (Int64)Convert.ChangeType(src, typeof(Int64));
             }
         }
-        /// <summary>
-        /// Converts a single-precision floating-point value into a 32-bit integer (将单精度浮点值转换为 32 位整数).
-        /// </summary>
-        /// <param name="value">The number to convert (欲转换的值).</param>
-        /// <returns>A 32-bit integer whose bits are identical to value (一个32位整数，表示转换的单精度浮点值).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Int32 SingleToInt32Bits(Single value) {
-#if (NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
-            return BitConverter.SingleToInt32Bits(value);
-#else
-            return Unsafe.As<Single, Int32>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Converts a single-precision floating-point value into a 32-bit unsigned integer (将指定的单精度浮点数转换为 32 位无符号整数).
-        /// </summary>
-        /// <param name="value">The number to convert (欲转换的值).</param>
-        /// <returns>A 32-bit integer whose bits are identical to value (一个32位无符号整数，表示转换的单精度浮点值).</returns>
-        [CLSCompliant(false)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static UInt32 SingleToUInt32Bits(Single value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.SingleToUInt32Bits(value);
-#else
-            return Unsafe.As<Single, UInt32>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Reinterprets the specified 32-bit signed integer value as a single-precision floating-point value (将指定的 32 位有符号整数值重新解释为单精度浮点值).
-        /// </summary>
-        /// <param name="value">The 32-bit signed integer value to convert (欲转换的32位带符号整数值).</param>
-        /// <returns>A single-precision floating-point value that represents the converted integer (表示根据整数转换后的单精度浮点值).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Single Int32BitsToSingle(Int32 value) {
-#if (NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
-            return BitConverter.Int32BitsToSingle(value);
-#else
-            return Unsafe.As<Int32, Single>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Reinterprets the specified 32-bit unsigned integer value as a single-precision floating-point value (将指定的 32 位无符号整数转换为单精度浮点数).
-        /// </summary>
-        /// <param name="value">The 32-bit unsigned integer value to convert (欲转换的32位无符号整数值).</param>
-        /// <returns>A single-precision floating-point value that represents the converted integer (表示根据整数转换后的单精度浮点值).</returns>
-        [CLSCompliant(false)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Single UInt32BitsToSingle(UInt32 value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.UInt32BitsToSingle(value);
-#else
-            return Unsafe.As<UInt32, Single>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-#if NET5_0_OR_GREATER
-        /// <summary>
-        /// Converts a half-precision floating-point value into a 16-bit integer (将半精度浮点值转换为 16 位整数).
-        /// </summary>
-        /// <param name="value">The number to convert (欲转换的值).</param>
-        /// <returns>A 16-bit integer whose bits are identical to value (一个16位整数，表示转换的半精度浮点值).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Int16 HalfToInt16Bits(Half value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.HalfToInt16Bits(value);
-#else
-            return Unsafe.As<Half, Int16>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Converts a half-precision floating-point value into a 16-bit unsigned integer (将指定的半精度浮点数转换为 16 位无符号整数).
-        /// </summary>
-        /// <param name="value">The number to convert (欲转换的值).</param>
-        /// <returns>A 16-bit integer whose bits are identical to value (一个16位无符号整数，表示转换的半精度浮点值).</returns>
-        [CLSCompliant(false)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static UInt16 HalfToUInt16Bits(Half value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.HalfToUInt16Bits(value);
-#else
-            return Unsafe.As<Half, UInt16>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Reinterprets the specified 16-bit signed integer value as a half-precision floating-point value (将指定的 16 位有符号整数值重新解释为半精度浮点值).
-        /// </summary>
-        /// <param name="value">The 16-bit signed integer value to convert (欲转换的16位带符号整数值).</param>
-        /// <returns>A half-precision floating-point value that represents the converted integer (表示根据整数转换后的半精度浮点值).</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Half Int16BitsToHalf(Int16 value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.Int16BitsToHalf(value);
-#else
-            return Unsafe.As<Int16, Half>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-        /// <summary>
-        /// Reinterprets the specified 16-bit unsigned integer value as a half-precision floating-point value (将指定的 16 位无符号整数转换为半精度浮点数).
-        /// </summary>
-        /// <param name="value">The 16-bit unsigned integer value to convert (欲转换的16位无符号整数值).</param>
-        /// <returns>A half-precision floating-point value that represents the converted integer (表示根据整数转换后的半精度浮点值).</returns>
-        [CLSCompliant(false)]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Half UInt16BitsToHalf(UInt16 value) {
-#if NET6_0_OR_GREATER
-            return BitConverter.UInt16BitsToHalf(value);
-#else
-            return Unsafe.As<UInt16, Half>(ref value);
-#endif // NET6_0_OR_GREATER
-        }
-
-#endif // NET5_0_OR_GREATER
 
         /// <summary>
         /// Get 64-bit's bits mask (取得64位的掩码).
@@ -505,12 +351,12 @@ namespace Zyl.VectorTraits {
                     SignBits = 1;
                     ExponentBits = 8;
                     MantissaBits = 23;
-                    SignMask = (T)(object)Scalars.Int32BitsToSingle((Int32)0x80000000);
-                    ExponentMask = (T)(object)Scalars.Int32BitsToSingle((Int32)0x7F800000);
-                    MantissaMask = (T)(object)Scalars.Int32BitsToSingle((Int32)0x007FFFFF);
-                    NonSignMask = (T)(object)Scalars.Int32BitsToSingle(~(Int32)0x80000000);
-                    NonExponentMask = (T)(object)Scalars.Int32BitsToSingle(~(Int32)0x7F800000);
-                    NonMantissaMask = (T)(object)Scalars.Int32BitsToSingle(~(Int32)0x007FFFFF);
+                    SignMask = (T)(object)BitUtil.Int32BitsToSingle((Int32)0x80000000);
+                    ExponentMask = (T)(object)BitUtil.Int32BitsToSingle((Int32)0x7F800000);
+                    MantissaMask = (T)(object)BitUtil.Int32BitsToSingle((Int32)0x007FFFFF);
+                    NonSignMask = (T)(object)BitUtil.Int32BitsToSingle(~(Int32)0x80000000);
+                    NonExponentMask = (T)(object)BitUtil.Int32BitsToSingle(~(Int32)0x7F800000);
+                    NonMantissaMask = (T)(object)BitUtil.Int32BitsToSingle(~(Int32)0x007FFFFF);
                     Epsilon = (T)(object)Single.Epsilon;
                     MaxValue = (T)(object)Single.MaxValue;
                     MinValue = (T)(object)Single.MinValue;
@@ -750,12 +596,12 @@ namespace Zyl.VectorTraits {
                     SignBits = 1;
                     ExponentBits = 5;
                     MantissaBits = 10;
-                    SignMask = (T)(object)Scalars.Int16BitsToHalf((Int16)0x8000);
-                    ExponentMask = (T)(object)Scalars.Int16BitsToHalf((Int16)0x7C00);
-                    MantissaMask = (T)(object)Scalars.Int16BitsToHalf((Int16)0x03FF);
-                    NonSignMask = (T)(object)Scalars.Int16BitsToHalf(~(Int16)0x8000);
-                    NonExponentMask = (T)(object)Scalars.Int16BitsToHalf(~(Int16)0x7C00);
-                    NonMantissaMask = (T)(object)Scalars.Int16BitsToHalf(~(Int16)0x03FF);
+                    SignMask = (T)(object)BitUtil.Int16BitsToHalf((Int16)0x8000);
+                    ExponentMask = (T)(object)BitUtil.Int16BitsToHalf((Int16)0x7C00);
+                    MantissaMask = (T)(object)BitUtil.Int16BitsToHalf((Int16)0x03FF);
+                    NonSignMask = (T)(object)BitUtil.Int16BitsToHalf(~(Int16)0x8000);
+                    NonExponentMask = (T)(object)BitUtil.Int16BitsToHalf(~(Int16)0x7C00);
+                    NonMantissaMask = (T)(object)BitUtil.Int16BitsToHalf(~(Int16)0x03FF);
                     Epsilon = (T)(object)Half.Epsilon;
                     MaxValue = (T)(object)Half.MaxValue;
                     MinValue = (T)(object)Half.MinValue;
