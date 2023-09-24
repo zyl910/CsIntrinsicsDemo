d4de37c326e9c19cf006882cdffb3cc7b4cd78e0
zhouyuelin <zyl910@sina.com>
2023-09-24 15:38:07 +0800

    NumberDivDemo.Run_Int16_Trun: Add y0IntFix .

--- MathDemo.Inc/NumberDivDemo.cs
+++ MathDemo.Inc/NumberDivDemo.cs
@@ -88,12 +88,13 @@ namespace MathDemo {
             long total = 0;
             long totalErrorYF = 0;
             long totalErrorY0 = 0;
+            long totalErrorY0Fix = 0;
             int diffMin = int.MaxValue;
             int diffMax = int.MinValue;
             int diff0Min = int.MaxValue;
             int diff0Max = int.MinValue;
             writer.WriteLine("## NumberDivDemo.Run_Int16_Trun");
-            writer.WriteLine(StringUtil.JoinTab("x", "b", "yInt", "yFloat", "y0", "yFInt", "y0Int", "yFDiff", "y0Diff"));
+            writer.WriteLine(StringUtil.JoinTab("x", "b", "yInt", "yFloat", "y0", "yFInt", "y0Int", "yFDiff", "y0Diff", "y0IntFix"));
             for (int i = 1; i < 0xFFFF; ++i) {
                 for (int j = 1; j < 0xFFFF; ++j) {
                     ++total;
@@ -108,6 +109,11 @@ namespace MathDemo {
                     float y0 = Volatile.Read(ref x) * Volatile.Read(ref d);
                     int y0Int = (int)Math.Truncate(Volatile.Read(ref y0));
                     int y0Diff = yInt - y0Int;
+                    int y0IntFix = y0Int;
+                    int x0Diff = i - (y0Int * j);
+                    if (Math.Abs(x0Diff)>= Math.Abs(j)) {
+                        ++y0IntFix;
+                    }
                     if (diffMin > yFDiff) diffMin = yFDiff;
                     if (diffMax < yFDiff) diffMax = yFDiff;
                     if (diff0Min > y0Diff) diff0Min = y0Diff;
@@ -115,12 +121,13 @@ namespace MathDemo {
                     // Show.
                     if (0 != yFDiff) ++totalErrorYF;
                     if (0 != y0Diff) ++totalErrorY0;
+                    if (yInt != y0IntFix) ++totalErrorY0Fix;
                     bool allowShow = true;
-                    allowShow = !(yInt == yFInt);
+                    allowShow = !(yInt == y0Int);
                     if (allowShow) {
                         if (showCount < StringUtil.MaxShowCount) {
                             ++showCount;
-                            string msg = StringUtil.JoinTab(x, b, yInt, ToStringWithHex(yFloat), ToStringWithHex(y0), yFInt, y0Int, yFDiff, y0Diff);
+                            string msg = StringUtil.JoinTab(x, b, yInt, ToStringWithHex(yFloat), ToStringWithHex(y0), yFInt, y0Int, yFDiff, y0Diff, y0IntFix);
                             writer.WriteLine(msg);
                         }
                     }
@@ -129,9 +136,16 @@ namespace MathDemo {
             writer.WriteLine(string.Format("total:\t{0}", total));
             writer.WriteLine(string.Format("totalErrorYF:\t{0}\t// {1:0.0000}%", totalErrorYF, (double)(100.0 * totalErrorYF) / total));
             writer.WriteLine(string.Format("totalErrorY0:\t{0}\t// {1:0.0000}%", totalErrorY0, (double)(100.0 * totalErrorY0) / total));
+            writer.WriteLine(string.Format("totalErrorY0Fix:\t{0}\t// {1:0.0000}%", totalErrorY0Fix, (double)(100.0 * totalErrorY0Fix) / total));
             writer.WriteLine(string.Format("diff:\t{0}~{1}", diffMin, diffMax));
             writer.WriteLine(string.Format("diff0:\t{0}~{1}", diff0Min, diff0Max));
             writer.WriteLine();
+            // total:  4294705156
+            // totalErrorYF:   0       // 0.0000%
+            // totalErrorY0:   32427   // 0.0008%
+            // totalErrorY0Fix:        0       // 0.0000%
+            // diff:   0~0
+            // diff0:  0~1
         }
 
     }
